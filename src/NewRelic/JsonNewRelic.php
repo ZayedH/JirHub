7b88d48f31ae00c358244d86ed10eadce53707e5
ZayedH <90324506+ZayedH@users.noreply.github.com>
2022-06-17 11:13:20 +0200

    Update src/NewRelic/JsonNewRelic.php
    Co-authored-by: Flavien Rodrigues <flavien.rodrigues@tiime.fr>

--- src/NewRelic/JsonNewRelic.php
+++ src/NewRelic/JsonNewRelic.php
@@ -29,7 +29,6 @@ class JsonNewRelic
                 ],
             ]                                                  
         );
-
         
         $content = $response->toArray();   
        
