c9fd46fffd912c42941187fc112e2a53d8734f35
ZayedH <hermazayed23@.com>
2022-06-27 10:45:09 +0200

    les packages

--- src/Garbage/OldLibraries.php
+++ src/Garbage/OldLibraries.php
@@ -11,13 +11,7 @@ class OldLibraries extends Command
 
     /** @var string */
     protected static $defaultName = 'collect:unupdated-libraries';
-
-    //private string $makeComposer;
-
-//    public function __construct()
-//    {
-//     parent::__construct();
-//    }
+    
     protected function configure()
     {
         $this->setDescription('collecting unupdated libraries');
@@ -25,36 +19,37 @@ class OldLibraries extends Command
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        
-        
-        //$result = shell_exec('composer outdated --format=json > src/Garbage/exemple.json'); ??????
-        // on va l'appeler à intervalles réguliers
-        $content = json_decode(file_get_contents('src/Garbage/exemple.json'),true);
-        $tab=['| Chronos (API) | version  | version disponible |',
-            '| --- | --- | --- |'];
-       foreach($content['installed'] as $value){
-        $name=$value['name'];
-        $version=$value['version'];
-        $latestVersion =$value['latest'];   
-        if($value['abandoned']){
-          
-           $tab[] ="| $name  | $version  | abandonné |";
-           
-        
-        }else{
-            
-               $tab[]= "| $name  | $version  | $latestVersion |";      
 
-            
-            
+
+
+        $content = json_decode(file_get_contents('src/Garbage/exemple.json'), true);
+        $tab = [];
+        foreach ($content['installed'] as $value) {
+            $name = $value['name'];
+            $version = $value['version'];
+            $latestVersion = $value['latest'];
+            $latestStatus = $value['latest-status'];
+            $isAbandoned = $value['abandoned'];
+            if ($isAbandoned || is_string($isAbandoned)) {
+
+                $tab[] = "| ⚠️" . " " . " $name  | $version  | abandonné |";
+            } else {
+
+                $pieces = explode('/', $name);
+                if ($pieces[0] == 'symfony') {
+                    if ($pieces[1] == "http-kernel") {
+                        array_unshift($tab, "| ⚠️" . " " . " $pieces[0]  | $version  | $latestVersion |");
+                    }
+                } else if ($latestStatus != 'semver-safe-update') {
+                    $tab[] = "| ⚠️" . " " . " $name  | $version  | $latestVersion |";
+                }
+            }
         }
 
-       }
-       $output->writeln($tab);
+        array_unshift($tab, '| Chronos (API) | version  | version disponible |', '| --- | --- | --- |');
+        $output->writeln($tab);
 
-        
+
         return 0;
     }
-
-    
 }
