7429e8e036b3e99e0b010e40a62954aaa0bf03a5
ZayedH <hermazayed23@.com>
2022-06-29 09:25:13 +0200

    CocoaPodsV0

--- src/NpmOutdated/test.php
+++ src/NpmOutdated/test.php
@@ -4,24 +4,14 @@ namespace App\NpmOutdated;
 
 class test
 {
-
     public function test()
     {
-     $fh = fopen('../src/NpmOutdated/fichier.txt', 'r');
         //$fh= shell_exec('ls');
         //dd($fh);
-        $array = explode("\n", file_get_contents('../src/NpmOutdated/fichier.txt'));
+        $array = explode("\n", file_get_contents('../src/NpmOutdated/ios.txt'));
         //$content = json_decode(file_get_contents('../src/NpmOutdated/exemple.json'),true);
         array_filter($array);
-        $num=count($array);
 
-        dd($array);
-
-       
-        
-       
-
-        
-       
+        dd(array_filter(explode(' ', $array[3])));
     }
 }
